edf69fa1f265a434898a2ff2a7ec188efecb6747
TuanKiet1774 <167632480+TuanKiet1774@users.noreply.github.com>
2025-02-15 12:59:16 +0700

    Add files via upload

---
+++ INFO_SECURITY/MaHoa_Ceasar.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+int main()
+{
+	int k;
+	char c[1000];
+	cin >> c;
+	cin >> k;
+	int n = strlen(c);
+	for(int i = 0; i<n; i++)
+	{
+		int temp = c[i] - k;
+		if(97 <= temp && temp <= 122)
+			c[i] = char(temp);
+		else if(temp < 97)
+				c[i] = char(temp + 26);
+		else if(temp > 122)
+				c[i] = char(temp - 26);
+	}
+	cout << c;
+}
