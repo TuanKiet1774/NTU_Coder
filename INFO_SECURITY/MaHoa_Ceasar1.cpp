4be2d0ad5e71c7078c4332d6bf005d613189c6d5
TuanKiet1774 <167632480+TuanKiet1774@users.noreply.github.com>
2025-02-20 21:29:44 +0700

    Update MaHoa_Ceasar1.cpp

--- INFO_SECURITY/MaHoa_Ceasar1.cpp
+++ INFO_SECURITY/MaHoa_Ceasar1.cpp
@@ -5,19 +5,18 @@ using namespace std;
 int main() {
     char c[100];
     int k;
-
-	gets(c);
+    gets(c);
     cin >> k; 
 
     for (int i = 0; c[i] != '\0'; i++) 
-	{
+    {
         if (c[i] >= 'A' && c[i] <= 'Z')
-		{  
+	{  
             c[i] += k;
             if (c[i] > 'Z') c[i] -= 26;
         } 
         else if (c[i] >= 'a' && c[i] <= 'z') 
-		{  
+	{  
             c[i] += k;
             if (c[i] > 'z') c[i] -= 26;  
         }
