f4e5d1d88ca1d91c2e10783daa9dc18798892afc
TuanKiet1774 <167632480+TuanKiet1774@users.noreply.github.com>
2025-02-20 21:28:20 +0700

    Add files via upload

---
+++ INFO_SECURITY/MaHoa_Ceasar1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main() {
+    char c[100];
+    int k;
+
+	gets(c);
+    cin >> k; 
+
+    for (int i = 0; c[i] != '\0'; i++) 
+	{
+        if (c[i] >= 'A' && c[i] <= 'Z')
+		{  
+            c[i] += k;
+            if (c[i] > 'Z') c[i] -= 26;
+        } 
+        else if (c[i] >= 'a' && c[i] <= 'z') 
+		{  
+            c[i] += k;
+            if (c[i] > 'z') c[i] -= 26;  
+        }
+    }
+    cout << c;
+}
+
