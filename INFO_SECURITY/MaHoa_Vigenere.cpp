591f42ad89029356cdd97fdc8db3ea6a56c29594
TuanKiet1774 <167632480+TuanKiet1774@users.noreply.github.com>
2025-02-27 22:52:34 +0700

    Add files via upload

---
+++ INFO_SECURITY/MaHoa_Vigenere.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string.h>
+#define max 1000
+using namespace std;
+
+void MaHoa_Vigenere(char text[], char k[], char Ma[]) 
+{
+	cin >> text;
+	cin >> k;
+	
+    int textLen = strlen(text);
+    int kLen = strlen(k);
+    
+    for (int i = 0; i < textLen; i++) 
+	{
+		int vt_k = i % kLen; // Vi tri ky tu cua moi ky tu trong khoa K (i luon < do dai chuoi)
+        char kChar = k[vt_k]; //Moi ky tu trong khoa K
+        
+        char P = text[i] - 'A'; 
+        char C = kChar - 'A';
+        
+        Ma[i] = (P + C) % 26 + 'A'; // C = (P + K) % 26
+    }
+    Ma[textLen] = '\0';
+}
+
+int main() 
+{
+    char text[max], k[max], Ma[max];
+    
+    MaHoa_Vigenere(text, k, Ma);
+    cout << Ma;
+}
