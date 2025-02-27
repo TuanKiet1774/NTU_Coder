a5fa167783bd51fd504fd3d7a12c8a7ba5cf0f37
TuanKiet1774 <167632480+TuanKiet1774@users.noreply.github.com>
2025-02-27 21:59:29 +0700

    Add files via upload

---
+++ INFO_SECURITY/PhaMa_DonBang.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <algorithm>
+#include <string.h>
+using namespace std;
+
+#define MAX_LEN 1000
+
+void PhaMa_DonBang(char Ma[MAX_LEN]) 
+{
+	cin >> Ma;
+    int BangChu[26] = {0}; // chua so lan xuat hien cua cac ky tu A -> Z
+    int n = strlen(Ma);
+    
+    for (int i = 0; i < n; i++) 
+    	BangChu[Ma[i] - 'A']++;
+    
+    int tu1 = -1, tu2 = -1, tu3 = -1; //So lan xuat hien nhieu nhat cua 3 tu dau tien
+    int vt1 = -1, vt2 = -1, vt3 = -1; //3 tu dau tien xuat hien nhieu nhat
+    
+    for (int i = 0; i < 26; i++) 
+	{
+        if (BangChu[i] > tu1) 
+		{
+            tu3 = tu2; 
+			vt3 = vt2;
+            tu2 = tu1; 
+			vt2 = vt1;
+            tu1 = BangChu[i]; 
+			vt1 = i;
+        } 
+		else if (BangChu[i] > tu2) 
+		{
+            tu3 = tu2; 
+			vt3 = vt2;
+            tu2 = BangChu[i]; 
+			vt2 = i;
+        } 
+		else if (BangChu[i] > tu3) 
+		{
+            tu3 = BangChu[i]; 
+			vt3 = i;
+        }
+    }
+    
+    cout << char(vt1 + 'A') << " " << tu1 << endl;
+    cout << char(vt2 + 'A') << " " << tu2 << endl;
+    cout << char(vt3 + 'A') << " " << tu3 << endl;
+    
+    for (int i = 0; i < n; i++) 
+	{
+        if (Ma[i] == vt1 + 'A') cout << 'E';
+        else if (Ma[i] == vt2 + 'A') cout << 'T';
+        else if (Ma[i] == vt3 + 'A') cout << 'A';
+        else cout << '-';
+    }
+    cout << endl;
+}
+
+int main() 
+{
+    char Ma[MAX_LEN];
+    PhaMa_DonBang(Ma);
+}
