7a71738c63ea9fd9f09205514fabddf873ff378f
TuanKiet1774 <167632480+TuanKiet1774@users.noreply.github.com>
2025-03-08 00:54:15 +0700

    Update MaHoa_Des.py

--- INFO_SECURITY/MaHoa_Des.py
+++ INFO_SECURITY/MaHoa_Des.py
@@ -1,3 +1,6 @@
+#Lưu ý: Cài thư viện PyCryptodomex
+#pip install pycryptodomex
+#pip install pycryptodome
 from Cryptodome.Cipher import DES
 
 def pad(tmp):
@@ -19,3 +22,11 @@ print()
 tmp = des.decrypt(cipher)  
 text = tmp.decode()
 print(text)
+
+#Ví dụ
+#Khóa K: hello123
+#Bản rõ: Python is the Best Language!
+#Kết quả
+#Chiều dài bản mã m = 32
+#Bản mã: 123 1 94 254 213 209 143 77 26 204 213 188 4 28 14 109 36 44 193 199 119 45 72 49 230 62 8 37 33 171 208 88
+#Bản giải mã: Python is the Best Language!
