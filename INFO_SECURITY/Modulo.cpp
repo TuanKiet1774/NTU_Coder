4f0668a6c5d78459bd138c41a6f1c6ebd35407ff
TuanKiet1774 <167632480+TuanKiet1774@users.noreply.github.com>
2025-04-09 00:33:05 +0700

    Update Modulo.cpp

--- INFO_SECURITY/Modulo.cpp
+++ INFO_SECURITY/Modulo.cpp
@@ -1,27 +1,22 @@
 #include <iostream>
 using namespace std;
 
-long long modulo(long long a, long long x, long long n) 
+int modulo(int a, int x, int n) 
 {
-    if (x == 0) 
-		return 1; 
-    
-    long long temp = modulo(a, x / 2, n);
-    long long kq;
-	kq = (temp * temp) % n; // temp * temp mod(n)
-
+    if (x == 0) return 1; 
+    int temp = modulo(a, x / 2, n);
+    int kq = (temp * temp) % n; // temp * temp mod(n)
     if (x % 2 == 1)
         kq = (kq * a) % n;
-
     return kq;
 }
 
 int main() 
 {
-    long long a, x, n;
+    int a, x, n;
     cin >> a >> x >> n;
     int m = modulo(a, x, n);
-    if(m < 0) cout << m + n;
+    if (m < 0) cout << m + n;
     else cout << m;
 }
 
