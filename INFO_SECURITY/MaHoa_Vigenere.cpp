e863e74181deca2249c968de6aad1ee0189083b3
TuanKiet1774 <167632480+TuanKiet1774@users.noreply.github.com>
2025-04-09 00:29:53 +0700

    Update MaHoa_Vigenere.cpp

--- INFO_SECURITY/MaHoa_Vigenere.cpp
+++ INFO_SECURITY/MaHoa_Vigenere.cpp
@@ -3,24 +3,18 @@
 #define max 1000
 using namespace std;
 
-void MaHoa_Vigenere(char text[], char k[]) 
-{
-	char Ma[max];
-	cin >> text;
-	cin >> k;
-	
+void MaHoa_Vigenere(char text[], char k[], char Ma[]) 
+{	
     int textLen = strlen(text);
     int kLen = strlen(k);
     
     for (int i = 0; i < textLen; i++) 
-	{
-		int vt_k = i % kLen; // Vi tri ky tu cua moi ky tu trong khoa K (i luon < do dai chuoi)
-        char kChar = k[vt_k]; //Moi ky tu trong khoa K
-        
-        char P = text[i] - 'A'; 
-        char C = kChar - 'A';
-        
-        Ma[i] = (P + C) % 26 + 'A'; // C = (P + K) % 26
+    {
+	int vt_k = i % kLen; // Vi tri ky tu cua moi ky tu trong khoa K (i luon < do dai chuoi)
+	char kChar = k[vt_k]; //Moi ky tu trong khoa K
+	char P = text[i] - 'A'; 
+	char K = kChar - 'A';
+	Ma[i] = (P + K) % 26 + 'A'; // C = (P + K) % 26
     }
     Ma[textLen] = '\0';
     cout << Ma;
@@ -28,9 +22,10 @@ void MaHoa_Vigenere(char text[], char k[])
 
 int main() 
 {
-    char text[max], k[max];
-    
-    MaHoa_Vigenere(text, k);
+    char text[max], k[max], Ma[max];
+    cin >> text;
+    cin >> k;
+    MaHoa_Vigenere(text, k, Ma);
 }
 
 //Ví dụ
